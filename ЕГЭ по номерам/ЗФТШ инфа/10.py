1569176f974a5821d77dda3fe76e7e70f3bc42fe
Shkityr Konstantin <81920594+Shkityrk@users.noreply.github.com>
2022-09-04 18:11:41 +0300

    test commit

--- ЕГЭ по номерам/ЗФТШ инфа/10.py
+++
@@ -1,8 +0,0 @@
-
-for m in range(1,10000):
-    for n in range(1,100):
-        l=m**2+n**2
-        k=m*n
-        if (m+n)**l==(l+2)**k:
-            print(m)
-            break
--- ЕГЭ по номерам/ЗФТШ инфа/7.py
+++
@@ -1,9 +0,0 @@
-def p(n):
-    s=[]
-    for x in range(1,n+1):
-        if n%x==0:
-            s.append(x)
-    return s
-print(sum(p(2021)))
-print(len(p(2021)))
-print(sum(p(2021))-len(p(2021)))
