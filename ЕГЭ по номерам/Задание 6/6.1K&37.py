28f32c84d4b7560957d06e73a4e8fdfc9836cc9b
Shkityr Konstantin <81920594+Shkityrk@users.noreply.github.com>
2022-09-26 22:08:58 +0300

    Решение задач №6 compege.ru

---
+++ ЕГЭ по номерам/Задание 6/6.1K&37.py
@@ -0,0 +1,14 @@
+from turtle import *
+screensize(10000,10000)
+tracer(0)
+r=3
+
+
+for i in range(44):
+    fd(10*r)
+    rt(8)
+
+up()
+
+update()
+exitonclick()
---
+++ ЕГЭ по номерам/Задание 6/6.2K.py
@@ -0,0 +1,13 @@
+from turtle import *
+screensize(10000,10000)
+tracer(0)
+r=10
+
+for i in range(100):
+    fd(10*r)
+    rt(30)
+
+up()
+
+update()
+exitonclick()
