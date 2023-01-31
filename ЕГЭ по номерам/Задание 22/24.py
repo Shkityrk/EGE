9b016ed2ff62b4b60cf68cc40c68fa277f407fd3
Shkityr Konstantin <81920594+Shkityrk@users.noreply.github.com>
2023-01-31 20:56:47 +0300

    Задание 22

---
+++ ЕГЭ по номерам/Задание 22/24.py
@@ -0,0 +1,8 @@
+d={'0':0}
+data=[s.replace(';',' ').split() for s in open("data/24.txt")]
+
+while len(d)!=len(data)+1:
+    for s in data:
+        if all(sub for sub in s[2:]):
+            d[s[0]]=int(s[1])+max(d[sub] for sub in s[2:])
+print(max(d.values()))
