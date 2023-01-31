9911d1467934d2e0832748777a274e8f281c0718
Shkityr Konstantin <81920594+Shkityrk@users.noreply.github.com>
2023-01-31 20:57:49 +0300

    changes

--- Other/tst.py
+++ Other/tst.py
@@ -1,10 +1,8 @@
-with open("D:\Библиотеки\OneDrive\Документы\dataset_3363_3.txt") as inf, open('MostPopularWord.txt','w') as ouf:
-    maxc = 0
-    s = inf.read().lower().strip().split()
-    s.sort()
-    for word in s:
-        counter = s.count(word)
-        if counter > maxc:
-            maxc = counter
-            result_word = word
-    ouf.write(result_word +' ' + str(maxc))
+from itertools import *
+def f(a,b,c,d):
+    return (a<=b) and (c<=d) or (not c)
+t=[(1,0,1,0), (0,0,1,1), (0,1,1,1), (1,0,1,1)]
+
+for p in permutations('abcd'):
+    if [f(**dict(zip(p,r))) for r in t]==[0,0,0,0]:
+        print(p)
