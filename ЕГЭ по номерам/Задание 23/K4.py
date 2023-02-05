06a3154bc54872b899d283b3fa837dbe6a161dad
Shkityr Konstantin <81920594+Shkityrk@users.noreply.github.com>
2023-02-05 17:10:38 +0300

    kompege.ru #23 часть 1

---
+++ ЕГЭ по номерам/Задание 23/K4.py
@@ -0,0 +1,5 @@
+def f(c,e):
+    if c<e: return 0
+    if c==e: return 1
+    if c>e: return f(c-2,e)+f(c-5,e)
+print(f(23,2))
---
+++ ЕГЭ по номерам/Задание 23/K8.py
@@ -0,0 +1,5 @@
+def f(c,e):
+    if c>e: return 0
+    if c==e: return 1
+    if c<e:return f(c+1,e)+f(c*2,e)+f(c+3,e)
+print(f(3,9)*f(9,12)*f(12,20))
